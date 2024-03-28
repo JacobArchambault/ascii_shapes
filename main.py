574840e0b3c4a7dfa634be8c1708ea2e1b3e07f5
Jacob Archambault <jacobarchambault@gmail.com>
2024-03-28 17:47:13 -0400

    make a diamond

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-astr = "*"
-for i in range(10):
-    for j in range(10 - i):
+length = 10
+asterisks = "*"
+for i in range(length):
+    for j in range(length - i):
         print(" ", end='')
-    print(astr)
-    astr += "**"
+    print(asterisks)
+    asterisks += "**"
+length -= 1
+space = 1
+asterisk_count = ((length * 2) - 1)
+asterisks = "*" * asterisk_count
+for i in range(length):
+    print(" " + space * " " + asterisks)
+    asterisk_count -= 2
+    asterisks = "*" * asterisk_count
+    space += 1
